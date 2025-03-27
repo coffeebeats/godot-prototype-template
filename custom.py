64da46e65b6f2fd2d32709dbb2260723837e853b
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2025-03-27 11:40:48 -0700

    feat: add basic `custom.py` for 2D games

---
+++ custom.py
@@ -0,0 +1,26 @@
+# A default 'custom.py' file suitable for 2D games.
+#
+# Generated using https://godot-build-options-generator.github.io
+
+disable_3d = "yes"
+deprecated = "no"
+minizip = "no"
+module_bullet_enabled = "no"
+module_camera_enabled = "no"
+module_csg_enabled = "no"
+module_dds_enabled = "no"
+module_enet_enabled = "no"
+module_gdnative_enabled = "no"
+module_gltf_enabled = "no"
+module_hdr_enabled = "no"
+module_jsonrpc_enabled = "no"
+module_mbedtls_enabled = "no"
+module_meshoptimizer_enabled = "no"
+module_mobile_vr_enabled = "no"
+module_multiplayer_enabled = "no"
+module_navigation_enabled = "no"
+module_upnp_enabled = "no"
+module_vhacd_enabled = "no"
+module_webrtc_enabled = "no"
+module_websocket_enabled = "no"
+module_webxr_enabled = "no"
